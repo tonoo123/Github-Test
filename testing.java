0463b0508f529805e26490c6a170006f3aec00df
tonoo123 <88918858+tonoo123@users.noreply.github.com>
2024-10-01 11:22:47 -0400

    Delete testing.java
    Deleted this test file, was not testing a lot. The other testing file (BinarySearchTreeTest.java) is more comprehensive.

--- testing.java
+++
@@ -1,8 +0,0 @@
-public class testing {
-    public static void main(String[] args) {
-        BinarySearchTree<String, String> tree = new BinarySearchTree<>();
-        tree.put( "Larry", "larry@mtu.edu" );
-        String value = tree.remove( "Larry" );
-        System.out.println(value);
-    }
-}
